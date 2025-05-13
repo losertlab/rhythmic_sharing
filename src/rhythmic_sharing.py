e49923ff0bfa55ac6ff4062df23c58285637a18f
Noah Chongsiriwatana <noahchongsiriwatana@Noahs-MacBook-Pro-6.local>
2025-05-12 22:23:29 -0400

    generate network objects.

--- src/rhythmic_sharing.py
+++ src/rhythmic_sharing.py
@@ -7,16 +7,18 @@ import networkx as nx
 class RhythmicNetwork:
     def __init__(self, **kwargs):
         self.tau = kwargs.get('tau', 1)
-        self.average_degree = kwargs.get('average_degree', 10)
+        self.average_degree_nodes = kwargs.get('average_degree_nodes', 10)
         self.num_nodes = kwargs.get('num_nodes', 100)
         self.link_dist = kwargs.get('link_dist', 'discrete')
         self.omega0 = kwargs.get('omega0', 0.01)
+        self.omega0_mean = kwargs.get('omega0_mean', self.omega0)
+        self.omega0_spread = kwargs.get('omega0_spread', self.omega0/3)
         self.input_weight = kwargs.get('input_weight', 120e-2)
         self.epsilon1 = kwargs.get('epsilon1', 0.2)
         self.epsilon2 = kwargs.get('epsilon2', 0.6)
         self.leakage = kwargs.get('leakage', 0.0)
         self.spectral_radius = kwargs.get('spectral_radius', 0.6)
-        self.bias = kwargs.get('bias', 0)
+        self.bias_nodes = kwargs.get('bias_nodes', 0)
         self.rhythmic_link_ratio = kwargs.get('rhythmic_link_ratio', 1)
         self.link_strength_change_ratio = kwargs.get('link_strength_change_ratio', 0.6)
         self.regularization = kwargs.get('regularization', 1e-20)
@@ -27,17 +29,19 @@ class RhythmicNetwork:
         self.average_degree_links = self.num_nodes // 2
         self.node_adj_matrix = self.gen_node_adj_matrix()
         self.nonzero_adj_idxs = np.where(np.ndarray.flatten(self.node_adj_matrix.toarray())!=0)[0]
-        self.incidence_matrix_T, self.incidence_normalization = self.gen_incidence_matrix_T()
+        self.node_incidence_T, self.node_incidence_norm = self.gen_node_incidence_T()
         self.input_weights = self.gen_input_weights()
         self.num_links = np.count_nonzero(self.node_adj_matrix.toarray())
+        self.link_adj_matrix, self.link_adj_norm = self.gen_link_adj_matrix()
+        self.natural_frequencies = self.gen_natural_frequencies()
 
     def gen_node_adj_matrix(self):
-        unbounded_links = sparse.random(self.num_nodes, self.num_nodes, density=self.average_degree/self.num_nodes, random_state=self.model_seed)
+        unbounded_links = sparse.random(self.num_nodes, self.num_nodes, density=self.average_degree_nodes/self.num_nodes, random_state=self.model_seed)
         bounded_links = 2*unbounded_links - unbounded_links.ceil()
         link_eigenvalues = linalg.eigs(bounded_links, k=1, return_eigenvectors=False)
         return self.spectral_radius/np.abs(link_eigenvalues[0])*bounded_links
 
-    def gen_incidence_matrix_T(self):
+    def gen_node_incidence_T(self):
         link_graph = nx.from_numpy_array(self.node_adj_matrix, parallel_edges=True, create_using=nx.DiGraph())
         nodelist = list(link_graph)
         if link_graph.is_multigraph():
@@ -77,3 +81,21 @@ class RhythmicNetwork:
             ip = 2*np.random.rand(qq) - 1
             input_weights[i*qq:(i+1)*qq, i] = self.input_weight*ip
         return input_weights
+
+    def gen_link_adj_matrix(self):
+        link_adj_matrix = sparse.csr_matrix.ceil(sparse.random(self.num_links, self.num_links, density=self.average_degree_links/self.num_links, random_state=self.model_seed+3))
+        link_adj_matrix_norm = np.sum(link_adj_matrix.toarray(), axis=1)
+        if np.all(link_adj_matrix.toarray()[np.where(link_adj_matrix_norm==0)]==0)==1:
+            link_adj_matrix_norm[np.where(link_adj_matrix_norm==0)]=1000
+        return link_adj_matrix, link_adj_matrix_norm
+
+    def gen_natural_frequencies(self):
+        if self.link_dist == 'discrete':
+            natural_frequencies = sparse.random(1, self.num_links, density=self.rhythmic_link_ratio, random_state=self.model_seed+5)
+            natural_frequencies = np.matrix.flatten(np.ceil(natural_frequencies.toarray()))*self.omega0
+        elif self.link_dist == 'normal':
+            natural_frequencies = sparse.random(1, self.num_links, density=self.rhythmic_link_ratio, random_state=self.model_seed+5)
+            natural_frequencies = np.matrix.flatten(np.ceil(natural_frequencies.toarray()))
+            natural_frequencies[np.where(natural_frequencies!=0)[0]] = np.random.normal(loc=self.omega0_mean, scale=self.omega0_spread, size=np.where(natural_frequencies!=0)[0].shape[0])
+        return natural_frequencies
+
